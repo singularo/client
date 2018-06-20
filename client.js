7384d5c7d73346d4317e0902e85a2a6375d31025
Chris Barton <cjbarton01@gmail.com>
2018-06-20 17:03:23 +0930

    Fix ws:// issue
    Determine the protocol and set the serverUrl path based on the result.

--- client.js
+++ client.js
@@ -1,7 +1,12 @@
 // Uncomment and fill out these if running the client stand-alone.
 // var serverUrl = "ws://server-url.net:5000";
 // var soundsUrl = "http://path-to-sounds.net/directory/";
-
+// If https Websocket secure can only be used.
+var protocol = (window.location.protocol === 'https:') ? 'wss://' : 'ws://'; 
+// Extract the protocol from the serverUrl path 
+var serverUrlPieces = serverUrl.split('//');
+// Assemble serverUrl path.
+serverUrl = protocol + serverUrlPieces[serverUrlPieces.length -1];
 var socket = new WebSocket(serverUrl);
 socket.onmessage = (message) => {
   data = message.data.trim();
